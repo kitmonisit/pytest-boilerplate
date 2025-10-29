af4b1183476fcd3ddcb3e3de06f43411bd645ddd
Kit Monisit <kmonisit@gmail.com>
2025-10-29 12:14:34 +0100

    Differentiate between passed, skipped, and failed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
+import logging
 import sys
 from copy import deepcopy
 from pathlib import Path
 from unittest import mock
 
 import pytest
+
+log = logging.getLogger(__name__)
 
 
 def _add_src_to_sys_path(session: pytest.Session):
@@ -13,7 +16,33 @@ def _add_src_to_sys_path(session: pytest.Session):
     return mock.patch(target="sys.path", new=mocked_sys_path)
 
 
+def _has_marker(item: pytest.Item, marker_name: str):
+    return marker_name in (marker.name for marker in item.own_markers)
+
+
 @pytest.hookimpl(wrapper=True)
 def pytest_collection(session: pytest.Session):
     with _add_src_to_sys_path(session):
         yield
+
+
+@pytest.hookimpl(wrapper=True)
+def pytest_runtest_makereport(item: pytest.Item, call: pytest.CallInfo):
+    result: pytest.TestReport = yield
+
+    if _has_marker(item, "skip") and call.when == "setup":
+        # A test decorated with @pytest.mark.skip will never get to a state where call.when == "call"
+        assert result.failed == False and result.outcome == "skipped", (
+            "If a test is decorated with @pytest.mark.skip, result.failed =="
+            f" False and result.outcome == 'skipped', but we have {result.failed} and"
+            f" '{result.outcome}' respectively. Something is wrong here."
+        )
+        log.warning("SKIPPED")
+
+    if call.when == "call":
+        if result.failed:
+            log.error("FAILED")
+        elif result.failed == False:
+            log.info("PASSED")
+
+    return result
--- tests/test_one.py
+++ tests/test_one.py
@@ -1,4 +1,44 @@
 import domain
+import pytest
 
-def test_one():
+
+def test_will_succeed():
     assert True
+
+
+@pytest.mark.xfail
+def test_will_succeed_unexpectedly():
+    assert True
+
+
+@pytest.mark.xfail
+def test_will_fail_as_expected():
+    assert False
+
+
+@pytest.mark.xfail
+def test_will_be_skipped_during_test_has_xfail():
+    pytest.skip()
+    assert False
+
+
+@pytest.mark.skip
+def test_will_be_skipped_as_marked():
+    assert False
+
+
+def test_will_be_skipped_during_test():
+    pytest.skip()
+    assert False
+
+
+@pytest.mark.parametrize(
+    ("value"),
+    (
+        True,
+        False,
+        pytest.param(False, marks=pytest.mark.xfail),
+    ),
+)
+def test_parametrize(value):
+    assert value
